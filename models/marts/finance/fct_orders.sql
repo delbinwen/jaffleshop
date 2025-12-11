c82d14bda84b49050d25c290df07ad158727bbc2
Wesley Wen <wesley.wen@coolbitx.com>
2025-12-11 03:11:55 +0000

    Update the models

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,24 +1,30 @@
-with customers as (
-    select * from {{ ref('stg_jaffle_shop__customers') }}
-),
-
-orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
 
 payments as (
-    select * from {{ ref('stg_stripe__payments') }}
+    select * from {{ ref ('stg_stripe__payments') }}
 ),
 
-final as (
-    select 
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
         orders.order_id,
-        customers.customer_id,
-        payments.amount 
-    from
-        orders
-    join customers on orders.customer_id = customers.customer_id
-    join payments on orders.order_id = payments.order_id
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
 select * from final
